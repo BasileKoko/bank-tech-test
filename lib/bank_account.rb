07c4433689b1cf0f3c78415ebac6496eff0d9e7d
Basile Koko <basile_koko@hotmail.com>
2017-03-09 12:23:13 +0000

    passing test respond to deposit money with 2 arguments

--- lib/bank_account.rb
+++ lib/bank_account.rb
@@ -1,7 +1,10 @@
 class BankAccount
   attr_reader :balance
-  
+
   def initialize
     @balance = 0
+  end
+
+  def deposit_money(sum, date)
   end
 end
--- spec/bank_account_spec.rb
+++ spec/bank_account_spec.rb
@@ -4,4 +4,5 @@ describe BankAccount do
   it "should create new bank_account with balance 0" do
     expect(subject.balance).to eq 0
   end
+  it {is_expected.to respond_to(:deposit_money).with(2)}
 end
