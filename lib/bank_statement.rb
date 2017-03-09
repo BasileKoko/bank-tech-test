5479e915c064fc29134e5b25899a049b26d24e73
Basile Koko <basile_koko@hotmail.com>
2017-03-09 13:45:58 +0000

    pass test access log array of a bank account from BankStatement class

--- lib/bank_statement.rb
+++ lib/bank_statement.rb
@@ -3,5 +3,6 @@ require_relative 'bank_account'
 class BankStatement
 
   def print_statment(bank_account)
+    log = bank_account.log
   end
 end
--- spec/bank_statement_spec.rb
+++ spec/bank_statement_spec.rb
@@ -1,6 +1,14 @@
 require 'bank_statement'
 
 describe BankStatement do
+  let(:bank_account) {double BankAccount.new}
+
   it {is_expected.to respond_to(:print_statment).with(1)}
 
+  it "access the log array of bank_account" do
+    allow(bank_account).to receive(:deposit_money).with(10, "09/03/2017")
+    allow(bank_account).to receive(:withdraw_money).with(5, "10/03/2017")
+    allow(bank_account).to receive(:log) {[["09/03/2017", 10, 0, 10], ["10/03/2017", 0, 5, 5]]}
+    expect(subject.print_statment(bank_account)).to include(["09/03/2017", 10, 0, 10], ["10/03/2017", 0, 5, 5])
+  end
 end
